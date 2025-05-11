cb410b46e6dd1ec0102000d0efdf7b33f26d98cb
v-chandana <chandanav778@gmail.com>
2025-05-11 20:48:03 +0530

    11-05-25

---
+++ src/basics/Iterations.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class Iterations {
+
+    public static void main(String[] args){
+        for( int i = 1;i<=10;i=i*2){
+            System.out.println(i);
+        }
+    }
+}
